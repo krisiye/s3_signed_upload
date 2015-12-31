18fb50a739c4b4e6d548d8c7e039e34f219f5ca0
krisiye <Krishnakumar.Iyer@hmhco.com>
2015-12-31 11:59:17 -0500

    Adding a prefix or a base dir to the key name to represent a site specific upload for instance. Also reading in AWS properties from the config.

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ var app = express()
 var AWS = require('aws-sdk');
 var bodyParser = require('body-parser');
 
-var AWS_ACCESS_KEY = 'XXXX';
-var AWS_SECRET_KEY = 'XXXX';
-AWS.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
-AWS.config.region = '';
+//var AWS_ACCESS_KEY = 'XXXX';
+//var AWS_SECRET_KEY = 'XXXX';
+//AWS.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
+//AWS.config.region = '';
+
+AWS.config.loadFromPath('./config/aws.json');
+
+var prefix_dir = 'site1/'; // represents the site context.
+var config = require('./config/aws.json');
 
 app.use("/app",express.static(__dirname + '/app'));
 app.use("/bower_components",express.static(__dirname + '/bower_components'));
@@ -19,18 +24,22 @@ app.use(bodyParser.json())
 
 app.post('/signedPUTUrl', function (req, res) {
 	var s3 = new AWS.S3();
-	var params = {Bucket: 'isg-spaces-nonprod-user-uploads', Key: req.body.name, ContentType: req.body.type};
+  var keyName = prefix_dir + req.body.name; 
+	var params = {Bucket: config.bucket, Key: keyName, ContentType: req.body.type};
 	s3.getSignedUrl('putObject', params, function(err, url) {
 		if(err) console.log(err);
+    console.log("PUT Url:" +url);
 		res.json({url: url});
 	});
 })
 
 app.post('/signedGETUrl', function (req, res) {
         var s3 = new AWS.S3();
-        var params = {Bucket: 'isg-spaces-nonprod-user-uploads', Key: req.body.name};
+        var keyName = prefix_dir + req.body.name; 
+        var params = {Bucket: config.bucket, Key: keyName};
         s3.getSignedUrl('getObject', params, function(err, url) {
                 if(err) console.log(err);
+                console.log("GET Url:" +url);
                 res.json({url: url});
         });
 })
